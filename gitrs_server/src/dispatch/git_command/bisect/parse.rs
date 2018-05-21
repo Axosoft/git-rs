6869844425cad097d0ead9d3d5227dbc77e61e52
Joshua Grosso <joshuag@axosoft.com>
2018-05-21 09:33:57 -0700

    Fix bisect step parsing

--- gitrs_server/src/dispatch/git_command/bisect/parse.rs
+++ gitrs_server/src/dispatch/git_command/bisect/parse.rs
@@ -1,26 +1,27 @@
+use nom::digit1;
 use util::parse::{parse_u32, sha};
 
 #[derive(Debug, Serialize)]
-pub struct StepOutput {
+pub struct BisectStep {
     current_commit_sha: String,
     num_revisions_left: u32,
     num_steps_left: u32,
 }
 
-named!(parse_start<&str, StepOutput>,
+named!(pub parse_bisect_step<&str, BisectStep>,
     do_parse!(
         tag!("Bisecting: ") >>
-        num_revisions_left: call!(parse_u32, 10) >>
+        num_revisions_left: digit1 >>
         tag!(" revision") >>
         opt!(char!('s')) >>
         tag!("left to test after this (roughly ") >>
-        num_steps_left: call!(parse_u32, 10) >>
+        num_steps_left: digit1 >>
         tag!(" steps)\n[") >>
         current_commit_sha: sha >>
-        (StepOutput {
+        (BisectStep {
             current_commit_sha: String::from(current_commit_sha),
-            num_revisions_left,
-            num_steps_left,
+            num_revisions_left: parse_u32(num_revisions_left, 10),
+            num_steps_left: parse_u32(num_steps_left, 10),
         })
     )
 );
